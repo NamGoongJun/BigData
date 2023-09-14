fea0c898750f2d59220263f49c1c2a54594c9bc7
201930211 <thfo121@naver.com>
2023-09-14 16:11:09 +0900

    week03 v1.7 2d array enter key

--- week03_numpy.py
+++ week03_numpy.py
@@ -2,7 +2,10 @@ import numpy as np
 import tkinter as tk
 from tkinter import messagebox
 
-def click_button(ev):
+def press_enter_key(ev):
+    click_button()
+
+def click_button():
     try:
         r, c = map(int, en_row_column.get().split()) # spacebar로 분리
         matrix = np.random.randint(1, 101, size=(r, c))
@@ -12,7 +15,7 @@ def click_button(ev):
         messagebox.showerror('Error', f"입력 값이 없습니다.\n{err}")
 
 window = tk.Tk()
-window.title('numpy gui version v1.6')
+window.title('numpy gui version v1.7')
 window.geometry('300x150')
 
 # create widget
@@ -21,7 +24,7 @@ en_row_column = tk.Entry()
 btn_click = tk.Button(text="click me!", command=click_button)
 
 # enter key binding with entry widget
-en_row_column.bind("<Return>", click_button)
+en_row_column.bind("<Return>", press_enter_key)
 
 # widget layout
 lbl_result.pack()
