8e84a5c06d7263f9d62563ed1554e2042eb211cb
201930211 <thfo121@naver.com>
2023-10-05 15:22:29 +0900

    week06 v2.5 module&class

--- tglearn.py
+++ tglearn.py
@@ -1,11 +1,9 @@
 import numpy as np
 
-
 class LinearRegression:
     def __init__(self):
         self.slope = None
         self.intercept = None
-
 
     def fit(self, X, y):
         """
@@ -16,10 +14,8 @@ class LinearRegression:
         """
         X_mean = np.mean(X)
         y_mean = np.mean(y)
-
-        denominator = np.sum(pow((X-X_mean), 2))
-        numerator = np.sum((X-X_mean)*(y-y_mean))
-
+        denominator = np.sum(pow((X - X_mean), 2))
+        numerator = np.sum((X - X_mean) * (y - y_mean))
         self.slope = numerator / denominator
         self.intercept = y_mean - (self.slope * X_mean)
 
