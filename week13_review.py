76d68ae3ac44e93d4477a58d336eadf34cd4f515
201930211 <thfo121@naver.com>
2023-11-23 16:33:36 +0900

    week13 v6.8

--- week13_review.py
+++ week13_review.py
@@ -1,19 +1,19 @@
-import numpy as np
 import seaborn as sns
 import pandas as pd
 
-a1 = np.ones(5)
-a2 = np.array([1, 1, 1, 1, 1])
-a3 = np.arange(5) * 0 + 1
-a4 = np.zeros(5) + 1
-a5 = np.linspace(1, 1, 5)
-a6 = np.full(5, 1)
-print(a1, a2, a3, a4, a5, a6)
 titanic = sns.load_dataset('titanic')
 
 print(titanic.isnull())
+print(titanic.isnull().sum())
+print(titanic['age'].fillna(titanic['age'].median(), inplace=True))
+print(titanic.isnull().sum())
+# titanic['deck'].fillna('Unknown', inplace=True)  # 범주형 카테고리에 Unknown이 없음
+titanic['deck'] = titanic['deck'].cat.add_categories('Unknown')  # 카테고리 추가
+titanic['deck'].fillna('Unknown', inplace=True)
 
 # print(type(titanic))
 # print(titanic.describe())
 # print(titanic.info())
 # print(titanic)
+# print(titanic.tail())
+print(titanic['deck'].tail(10))
